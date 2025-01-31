a4d5889e6d4818017f1fac40c4d0e406b57c8363
Zaradin <crottyjj@hotmail.com>
2025-01-31 11:58:19 +0000

    Demo optional properties and parameters.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,59 +1,91 @@
-import {Friend, Colleague, EmailContact } from './myTypes'
+import { Friend, Colleague, EmailContact } from "./myTypes";
 import { friends, colleagues } from "./01-basics";
 
-function older(f: Friend) : string {
-    f.age += 1
-    return `${f.name} is now ${f.age}` 
+function older(f: Friend): string {
+    f.age += 1;
+    return `${f.name} is now ${f.age}`;
 }
 
-console.log(older(friends[0]))
+console.log(older(friends[0]));
 
 function allOlder(friends: Friend[]): string[] {
-    return friends.map((friend) => older(friend))
+    return friends.map((friend) => older(friend));
 }
 
-console.log(allOlder(friends))
+console.log(allOlder(friends));
 
 // Find the colleague with the highest extension number.
-function highestExtension(cs: Colleague[]) { // Inferred retun type
+function highestExtension(cs: Colleague[]) {
+    // Inferred retun type
     const result = cs.sort(
-      (c1, c2) => c1.contact.extension - c2.contact.extension
+        (c1, c2) => c1.contact.extension - c2.contact.extension
     );
     return result[cs.length - 1];
 }
-  
+
 console.log(highestExtension(colleagues.current));
-  
-function addColleague(colleagues: Colleague[], name: string, department: string, email: string): void {
+
+function addColleague(
+    colleagues: Colleague[],
+    name: string,
+    department: string,
+    email: string
+): void {
     const highestExt = highestExtension(colleagues).contact.extension;
     const newColleague: Colleague = {
-      name,
-      department,
-      contact: {
-        email,
-        extension: highestExt + 1,
-      },
+        name,
+        department,
+        contact: {
+            email,
+            extension: highestExt + 1,
+        },
     };
     colleagues.push(newColleague);
 }
-  
+
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
 function sortColleagues(
     colleagues: Colleague[],
-    sorter: (c1: Colleague, c2: Colleague) => number
-  ): EmailContact[] {
-    const sorted = colleagues.sort(sorter); // Colleague[] inferred
-    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
-    return result 
+    sorter: (c1: Colleague, c2: Colleague) => number,
+    max?: number
+): EmailContact[] {
+    let end = colleagues.length;
+    if (max !== undefined) {
+        end = max < 2 ? 1 : max;
+    }
+    const sorted = colleagues.sort(sorter);
+    const fullResult = sorted.map((ce) => ({
+        name: ce.name,
+        email: ce.contact.email,
+    }));
+    return fullResult.slice(0, end);
 }
-  
-  console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
-  console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
-  
-function findFriends(friends: Friend[], criterion: (friend: Friend) => boolean): string[] {
+// Test invocations
+console.log(
+    sortColleagues(
+        colleagues.current,
+        (a, b) => a.contact.extension - b.contact.extension,
+        3
+    )
+);
+console.log(
+    sortColleagues(
+        colleagues.current,
+        (a, b) => a.name.length - b.name.length,
+        1
+    )
+);
+console.log(
+    sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length)
+); // NEW
+
+function findFriends(
+    friends: Friend[],
+    criterion: (friend: Friend) => boolean
+): string[] {
     return friends.filter(criterion).map((friend) => friend.name);
 }
-console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.name.startsWith("Pa")));
 console.log(findFriends(friends, (friend) => friend.age < 35));
--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -1,26 +1,26 @@
-
 export interface Friend {
     name: string;
     phone: string;
-    age: number
+    dob?: Date;
+    age: number;
+    interests?: string[];
 }
 
 export interface Colleague {
     name: string;
     department: string;
     contact: {
-      email: string;
-      extension: number
-    } 
-  }
+        email: string;
+        extension: number;
+    };
+}
 
 export interface ColleagueHistory {
-  current: Colleague[],
-  former: Colleague[]
+    current: Colleague[];
+    former: Colleague[];
 }
 
 export interface EmailContact {
     name: string;
-    email: string
+    email: string;
 }
-
